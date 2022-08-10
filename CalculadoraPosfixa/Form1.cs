e885bbf05fc1a03d95b84ed8ba14c3f1e821c0cb
Jhon <jw.jhonatan1711@gmail.com>
2022-08-09 21:57:14 -0300

    att

--- CalculadoraPosfixa/Form1.cs
+++ CalculadoraPosfixa/Form1.cs
@@ -23,22 +23,38 @@ namespace CalculadoraPosfixa
             {
                 char numero = txtVisor.Text[i]; //numero recebe o que foi digitado no txtCodigo
 
-                if (char.IsLetter(numero)) //se o que foi digitado no txtCodigo não seja numero
+                if (char.IsLetter(numero) || "!@#$%&".Contains(numero)) //se o que foi digitado no txtCodigo não seja numero
                 {
                     MessageBox.Show("Digite um valor válido!(Sem letras e sem caracteres que não fazem uma operação matemática!)"); //um aviso será disparado
                     txtVisor.Text = txtVisor.Text.Substring(0, txtVisor.Text.Length - 1);
-                    txtVisor.Focus();
+                }
+                else if (")]".Contains(txtVisor.Text.Substring(0,1))) {
+                    MessageBox.Show("Expressões iniciada de maneira incorreta");
+                    txtVisor.Text = txtVisor.Text.Substring(0, txtVisor.Text.Length - 1);
                 }
             }
         }
 
-        private void botoes_Click(object sender, EventArgs e)
-        {
+        private void botoes_Click(object sender, EventArgs e)        {
             char letra = (sender as Button).Text[0];
             txtVisor.Text += letra.ToString();
         }
         private void btnIgual_Click(object sender, EventArgs e)
         {
+            if (txtVisor.Text != "")
+            {
+                string expressao = txtVisor.Text;
+                string[] valores = new string[] { txtVisor.Text};
+                PilhaLista<char> pilhaExpressoes = new PilhaLista<char>();
+                MessageBox.Show(valores[0]);
+                
+            }
+            else {
+                MessageBox.Show("Digite a expressão!");
+                txtVisor.Focus();
+            }
+
+
             double resultado = 1;
             txtResultado.Text = resultado.ToString(); ;
             lbSequencias.Text = "A sequência...";
@@ -48,6 +64,13 @@ namespace CalculadoraPosfixa
             txtVisor.Clear();
             txtResultado.Clear();
             lbSequencias.Text = "Pósfixa ";
+        }
+
+        private void txtVisor_Leave(object sender, EventArgs e)
+        {
+            PilhaLista<char> pilha = new PilhaLista<char>();
+            string expressao = txtVisor.Text;
+            //BALANCEAMENTO DE PARENTESES
         }
     }
 }
