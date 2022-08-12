78a9aebd16b57df25ff3423dbfcfe72908011714
Jhon <jw.jhonatan1711@gmail.com>
2022-08-12 08:13:45 -0300

    colocando valores no vetor

--- CalculadoraPosfixa/Form1.Designer.cs
+++ CalculadoraPosfixa/Form1.Designer.cs
@@ -90,9 +90,9 @@ namespace CalculadoraPosfixa
             this.lbSequencias.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbSequencias.Location = new System.Drawing.Point(12, 153);
             this.lbSequencias.Name = "lbSequencias";
-            this.lbSequencias.Size = new System.Drawing.Size(70, 24);
+            this.lbSequencias.Size = new System.Drawing.Size(129, 24);
             this.lbSequencias.TabIndex = 3;
-            this.lbSequencias.Text = "Pósfixa";
+            this.lbSequencias.Text = "Pósfixa - Infixa";
             // 
             // btnPotencia
             // 
